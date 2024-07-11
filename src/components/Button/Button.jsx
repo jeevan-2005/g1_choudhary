d76a1023865d54e1f4c7032203a812fc9df8e267
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-07-11 19:53:37 +0530

    button component added.

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,30 @@
+import { useContext } from "react";
+import "./button.scss";
+import { Button } from "@mui/material";
+import { ThemeContext } from "../../context/ThemeContext";
+
+const MyButton = ({ text, href, newTab, className }) => {
+  const { theme } = useContext(ThemeContext);
+
+  const handleOnClick = (href)=>{
+    if(!href){
+      console.log(`Url not found...`);
+      return;
+    }
+    console.log(href);
+    window.open(href, "_blank", 'noopener,noreferrer');
+  }
+
+  return (
+    <div>
+      <Button onClick={()=>handleOnClick(href)}  className="btn"  sx={{
+        color: theme === "dark"? "rgb(0,0,0)" : "rgb(207,207,207)" ,
+        backgroundColor: theme === "dark" ? "rgb(0, 255, 255)" : "rgb(0,28,85)",
+      }}  >
+        {text}
+      </Button>
+    </div>
+  );
+};
+
+export default MyButton;
