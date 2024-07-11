1f77c9f8b6761dd1fd7a746f2b80ff294513697f
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-07-11 20:00:26 +0530

    added provider...

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react";
+// import { ChakraProvider } from "@chakra-ui/react";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { DurationProvider } from "./context/DurationContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <ThemeProvider>
+      <DurationProvider>
+        <App />
+      </DurationProvider>
+    </ThemeProvider>
   </BrowserRouter>
 );
