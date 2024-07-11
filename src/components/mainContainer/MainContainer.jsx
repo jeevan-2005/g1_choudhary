b5de7c3ffa6e2c2084c91bd88dfb502245370bb0
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-07-11 20:02:32 +0530

    added animation to the container...

--- src/components/mainContainer/MainContainer.jsx
+++ src/components/mainContainer/MainContainer.jsx
@@ -1,16 +1,29 @@
 import "./_mainContainer.scss";
 import AppRoutes from "../../routes/AppRoutes";
 import "animate.css";
+import { useEffect, useState } from "react";
+import { useSpring, animated } from "@react-spring/web";
 
 const MainContainer = () => {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    // Simulate loading complete after a delay
+    const timer = setTimeout(() => setLoaded(true), 1500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  const styles = useSpring({ 
+    opacity: loaded ? 1: 0, 
+    config: { tension: 170, friction: 60 }, // Smooth spring settings
+  });
+
   return (
-    <div className="main-content">
-      <div className="gradientBorder animate__animated animate__fadeIn">
-        <div className="pageContent">
-          <AppRoutes />
-        </div>
+    <animated.div style={styles} className="main-content">
+      <div className="pageContent">
+        <AppRoutes />
       </div>
-    </div>
+    </animated.div>
   );
 };
 
