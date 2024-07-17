c94a0eed9f4d64ab1e592fb1efc288ad7b7ff2a4
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-07-17 22:02:56 +0530

    Education component added...

--- src/components/PageComponents/Education/Education.jsx
+++ src/components/PageComponents/Education/Education.jsx
@@ -1,11 +1,26 @@
-import './_education.scss';
+import { education } from "../../../portfolio";
+import Heading from "../../heading/Heading1/Heading";
+import "./_education.scss";
 
 const Education = () => {
   return (
-    <div className='educationPage'>
-      Education
+    <div className="educationPage">
+      <Heading text={education.heading} />
+      <div className="timeline">
+        {education.institutes.map((institute, i) => (
+          <div className={institute.className} key={i}>
+            <img src={institute.imgSrc} alt={institute.imgAlt} />
+            <div className="aboutEducation">
+              <h2>{institute.name}</h2>
+              <small>
+                {institute.from}-{institute.to}
+              </small>
+              <p className="description">{institute.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
-
-export default Education
+  );
+};
+export default Education;
