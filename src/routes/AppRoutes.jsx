cea8d6a9de55e74fc984cf22b5428290a663dc1a
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-07-18 18:45:29 +0530

    ScrollToTop component added to route...

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,20 +1,24 @@
-import { Route, Routes } from 'react-router-dom'
-import Home from '../components/PageComponents/Home/Home'
-import Skills from '../components/PageComponents/Skills/Skills'
-import Education from '../components/PageComponents/Education/Education'
-import Projects from '../components/PageComponents/Projects/Projects'
-import Contact from '../components/PageComponents/Contact/Contact'
+import { Route, Routes } from "react-router-dom";
+import Home from "../components/PageComponents/Home/Home";
+import Skills from "../components/PageComponents/Skills/Skills";
+import Education from "../components/PageComponents/Education/Education";
+import Projects from "../components/PageComponents/Projects/Projects";
+import Contact from "../components/PageComponents/Contact/Contact";
+import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 
 const AppRoutes = () => {
   return (
-    <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/education' element={<Education/>} />
-        <Route path='/skills' element={<Skills/>} />
-        <Route path='/projects' element={<Projects/>} />
-        <Route path='/contact' element={<Contact/>} />
-    </Routes>
-  )
-}
+    <>
+       <ScrollToTop containerSelector=".main-content" />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
