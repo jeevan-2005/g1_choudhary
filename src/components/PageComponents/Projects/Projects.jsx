70db239cb8f0731500573a6a3b013602e78f068e
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-07-20 17:55:53 +0530

    projects component added...

--- src/components/PageComponents/Projects/Projects.jsx
+++ src/components/PageComponents/Projects/Projects.jsx
@@ -1,11 +1,17 @@
-import './_projects.scss';
+import Heading from "../../heading/Heading1/Heading";
+import "./_projects.scss";
+import ProjectSection from "./projectsComponents/ProjectSection/ProjectSection";
 
 const Projects = () => {
-  return (
-    <div className='projectsPage'>
-      Projects
-    </div>
-  )
-}
+  
 
-export default Projects
+
+  return (
+    <div className="projectsPage">
+      <Heading text="Projects" />
+      <ProjectSection/>
+    </div>
+  );
+};
+
+export default Projects;
