d8c6a2a5d291b49e59d24ac1fc3dd7f324ee2e91
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-07-21 17:15:21 +0530

    Chakra-Ui skill svg added...

---
+++ src/assets/skillsSvg/ChakraSvg.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const ChakraSvg = (props) => {
+  return (
+    <svg
+    fill="#4ac8c2"
+    viewBox="0 0 24.00 24.00"
+    role="img"
+    xmlns="http://www.w3.org/2000/svg"
+    stroke="#4ac8c2"
+    strokeWidth={0.00024000000000000003}
+    height="100%"
+    width="100%"
+    {...props}
+  >
+    <g id="SVGRepo_bgCarrier" strokeWidth={0} />
+    <g
+      id="SVGRepo_tracerCarrier"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <g id="SVGRepo_iconCarrier">
+      <path d="M12 0C5.352 0 0 5.352 0 12s5.352 12 12 12 12-5.352 12-12S18.648 0 12 0zm2.8 4.333c.13-.004.248.136.171.278l-3.044 5.58a.187.187 0 0 0 .164.276h5.26c.17 0 .252.207.128.323l-9.22 8.605c-.165.154-.41-.063-.278-.246l4.364-6.021a.187.187 0 0 0-.151-.296H6.627a.187.187 0 0 1-.131-.32l8.18-8.123a.182.182 0 0 1 .125-.056z" />
+    </g>
+  </svg>
+  )
+}
+
+export default ChakraSvg
