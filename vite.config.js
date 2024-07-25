318e58d427b3b6f08d0453150f7a03e1a114c7eb
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-07-25 16:56:39 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/'  // Replace with your repository name
+  base: '/g1_choudhary/',  // Replace with your repository name
+  plugins: [react()]
 });
