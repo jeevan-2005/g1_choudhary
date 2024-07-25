9ab0bd2792db772697110ec497bd4551b4efa21c
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-07-25 15:59:45 +0530

    loader added to the component till the data gets fetched...

--- src/components/PageComponents/Projects/projectsComponents/ProjectSection/ProjectSection.jsx
+++ src/components/PageComponents/Projects/projectsComponents/ProjectSection/ProjectSection.jsx
@@ -15,14 +15,14 @@ const ProjectSection = () => {
     dispatch(fetchGithubStats());
   }, [dispatch]);
 
-  const handleImageOnClick = (href)=>{
-    if(!href){
+  const handleImageOnClick = (href) => {
+    if (!href) {
       console.log(`Url not found...`);
       return;
     }
     console.log(href);
-    window.open(href, "_blank", 'noopener,noreferrer');
-  }
+    window.open(href, "_blank", "noopener,noreferrer");
+  };
 
   const projectsDemoLink = {
     "Bellavita-Clone": {
@@ -45,61 +45,68 @@ const ProjectSection = () => {
 
   return (
     <div className="allProjects">
-      {data.map((project, i) => (
-        <div key={i} className="singleProject">
-          <div className="projectCard">
-            {loading ? (
-              <CircularProgress color="inherit" />
-            ) : (
-              <>
-                <h2>{project.name}</h2>
-                <p>{project.description}</p>
-                <li>
-                  <span
-                    style={{
-                      display: "inline-block",
-                      width: "8px",
-                      height: "8px",
-                      backgroundColor: project.primaryLanguage.color,
-                      borderRadius: "50%",
-                      marginRight: "10px",
-                    }}
-                  ></span>
-                  {project.primaryLanguage.name}
-                </li>
-                <div className="projectLinks">
-                  <a
-                    href={projectsDemoLink[project.name].demo}
-                    target="_blank"
-                  >
-                    🌐Demo
-                  </a>
-                  <a href={project.url} target="_blank">
-                    🔗Github
-                  </a>
-                </div>
-              </>
-            )}
-          </div>
-          <Tilt
-            key={i}
-            perspective={1000}
-            scale={1}
-            tiltAngleXInitial={0}
-            tiltAngleYInitial={-10}
-            className="parallax-effect"
-          >
-            <div className="projectImage" onClick={()=>handleImageOnClick(projectsDemoLink[project.name].demo)}>
-              <img
-                src={projectsDemoLink[project.name].img}
-                alt={project.name}
-                ref={projectsDemoLink[project.name].ref}
-                style={projectsDemoLink[project.name].style}
-              />
-            </div>
-          </Tilt>
+      {loading ? (
+        <div style={{
+          display:"flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height:"50vh",
+        }}>
+          <CircularProgress size={"3rem"} thickness={3.6}/>
         </div>
-      ))}
+      ) : (
+        data.map((project, i) => (
+          <div key={i} className="singleProject">
+            <div className="projectCard">
+              <h2>{project.name}</h2>
+              <p>{project.description}</p>
+              <li>
+                <span
+                  style={{
+                    display: "inline-block",
+                    width: "8px",
+                    height: "8px",
+                    backgroundColor: project.primaryLanguage.color,
+                    borderRadius: "50%",
+                    marginRight: "10px",
+                  }}
+                ></span>
+                {project.primaryLanguage.name}
+              </li>
+              <div className="projectLinks">
+                <a href={projectsDemoLink[project.name].demo} target="_blank">
+                  🌐Demo
+                </a>
+                <a href={project.url} target="_blank">
+                  🔗Github
+                </a>
+              </div>
+            </div>
+            <Tilt
+              key={i}
+              perspective={1000}
+              scale={1}
+              tiltAngleXInitial={0}
+              tiltAngleYInitial={-10}
+              className="parallax-effect"
+            >
+              <div
+                className="projectImage"
+                onClick={() =>
+                  handleImageOnClick(projectsDemoLink[project.name].demo)
+                }
+              >
+                <img
+                  src={projectsDemoLink[project.name].img}
+                  alt={project.name}
+                  ref={projectsDemoLink[project.name].ref}
+                  style={projectsDemoLink[project.name].style}
+                />
+              </div>
+            </Tilt>
+          </div>
+        ))
+      )}
     </div>
   );
 };
