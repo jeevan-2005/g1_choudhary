009e8af24fed271ca6da6345de5a00afecfdc88a
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-07-25 19:59:01 +0530

    vite.config.js added...

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/g1_choudhary/',  // Replace with your repository name
   plugins: [react()]
 });
